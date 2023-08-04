07851b0394480b5d113283f6250c1884419fd9ad
PNS212624\Hansol <yrlee00@hansol.com>
2023-08-04 11:18:52 +0900

    2023.08.04 html 인프런 실습용 파일 생성

---
+++ src/test/java/com/example/tag/TagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
